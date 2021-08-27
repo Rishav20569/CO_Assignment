a21aaffd67d738dd3dc133b97181140d5afb710d
Rishav <rishav20569@iiitd.ac.in>
2021-08-27 23:52:53 +0530

    End

--- CO_M21_Assignment-4be702828e30f91fab146f7672399e9419674bb7/Simple-Assembler/co_assignment_assembler.py
+++ CO_M21_Assignment-4be702828e30f91fab146f7672399e9419674bb7/Simple-Assembler/co_assignment_assembler.py
@@ -1,34 +1,31 @@
 # CO project(semester 2) to design a custom assembler 
 #group members: Aditya, Rishav,Samuel
-
-
 import sys
 assemblyfile=sys.stdin.read().splitlines()
-
-# custom input 
-# with open("test1.txt") as r:
-#    assemblyfile=r.read().splitlines()
-
-
+# with open("test2.txt") as r:
+    # assemblyfile=r.read().splitlines()
 
 REG_ADD={"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101","R6":"110"}
 Flags_reg={"FLAGS":"111"}
 Instruction={"add":["00000","A"],"sub":["00001","A"],"mov":["00010","B"],"mov1":["00011","C"],"ld":["00100","D"],"st":["00101","D"],"mul":["00110","A"],"div":["00111","C"],"rs":["01000","B"],"ls":["01001","B"],"xor":["01010","A"],"or":["01011","A"],"and":["01100","A"],"not":["01101","C"],"cmp":["01110","C"],"jmp":["01111","E"],"jlt":["10000","E"],"jgt":["10001","E"],"je":["10010","E"],"hlt":["10011","F"],}
 
+#to store instruction address
 Address=[]
+#stores variables
 variable=[]
+
+#stores variable count or address
 Var={}
 
-
+# function to convert decimal to binary of 8bits
 def Bin(x):
     b=bin(x)[2:]
     a=(8-len(b))*"0"+str(b)
     return a
 
-
-
 flag = 0
 add_count=0
+
 #counting instructions other than var.
 for line in assemblyfile:
     if(len(line)==0):continue
@@ -45,7 +42,7 @@ y=add_count
 for line in assemblyfile:
     if(len(line)==0):continue
     y=list(line.split())
-    if y[0]=="var":
+    if y[0]=="var" and len(y)==2:
         Var[y[1]]=add_count
         add_count+=1
 
@@ -54,8 +51,11 @@ for i in range(0,len(variable)-1):
     if(len(line)==0):continue
     if variable[i]!="var":
         if variable[i+1]=="var":
-            print("Variables not declared at beginning.")
+            print("Line "+ str(add_count-1)+":"+" Variables not declared at beginning.")
+            flag=1
+            break
 
+#adding labels in this list
 Label_add={}
 c=-1
 for line in assemblyfile:   # will store add of labels
@@ -65,13 +65,13 @@ for line in assemblyfile:   # will store add of labels
     if y[0][-1]==":":
         Label_add[y[0]]=c
 
-
 c=0
 if add_count>256:
     print("Lines in code exceed 256 limit.")
-
+    flag=1
 else:    
     #code to print errors if any
+    
     for line in assemblyfile:
         c+=1
         if(len(line)==0):continue
@@ -83,25 +83,32 @@ else:
        
 
         if x[0]=="var":
-            continue    
+            if(len(x)==1):
+                print("Line"+" "+str(add_count-1)+":"+"General syntax error")
+                flag=1
         else:
             Address.append(add_count)
-            add_count+=1
+            
             
             if x[0][-1]==":":
                 print(end='')
             else:
+                if x[0] not in Instruction.keys():
+                    print("Line"+" "+str(add_count-1)+":"+"General syntax error")
+                    flag=1
+                    break
                 #for Type A instruction
                 if x[0]=="add" or x[0]=="sub" or x[0]=="mul" or x[0]=="xor" or x[0]=="or" or x[0]=="and":                
-                        if len(x)==5:
+                    if len(x)==4:
 
-                            if(x[1] not in REG_ADD.keys() or x[2] not in REG_ADD.keys() or x[3] not in REG_ADD.keys()):
-                                flag=1
-                                if x[1]=="FLAGS" or x[2]=="FLAGS" or x[3]=="FLAGS":
-                                    print("Illegal use of FLAGS register.")
-                                else:
-                                    print("Typos in register name.")
-                
+                        if(x[1] not in REG_ADD.keys() or x[2] not in REG_ADD.keys() or x[3] not in REG_ADD.keys()):
+                            flag=1
+                            if x[1]=="FLAGS" or x[2]=="FLAGS" or x[3]=="FLAGS":
+                                print("Line"+" "+str(add_count-1)+":"+"Illegal use of FLAGS register.")
+                            else:
+                                print("Line"+" "+str(add_count-1)+":"+"Typos in register name.")
+                            break
+
                 #for Type B instruction
                 if (x[0]=="mov" and x[2][0]=="$") or x[0]=="rs" or x[0]=="ls":
                     if (x[1] in REG_ADD.keys()):
@@ -109,14 +116,15 @@ else:
                         z=int(x[2][1:])
                         if(z<0 or z>255):
                             flag=1
-                            print("Illegal immediate values.")
+                            print("Line"+" "+str(add_count-1)+":"+"Illegal immediate values.")
+                            break
                     else:
                         flag=1
                         if x[1]=="FLAGS" :
-                                    print("Illegal use of FLAGS register.")
+                                    print("Line"+" "+str(add_count-1)+":"+"Illegal use of FLAGS register.")
                         else:
-                            print("Typos in register name.")
-                
+                            print("Line"+" "+str(add_count-1)+":"+"Typos in register name.")
+                        break
                 if(x[0]=="mov" and x[2][0]!="R"):
                     if (x[2][0]=="FLAGS" ):
                             continue
@@ -124,17 +132,17 @@ else:
                      #   if(x[2][0]!="$"):    
                       #      print("General Syntax error.")
 
-
                 #for Type C instruction
                 if (x[0]=="mov" and x[2][0]=="R") or x[0]=="div" or x[0]=="cmp" or x[0]=="not":
 
                     if(x[1] not in REG_ADD.keys() and x[2] not in REG_ADD.keys()):
                         flag=1
                         if x[1]=="FLAGS" :
-                            print("Illegal use of FLAGS register.")
+                            print("Line"+" "+str(add_count-1)+":"+"Illegal use of FLAGS register.")
                         else:
-                            print("Typos in register name.")                
-                
+                            print("Line"+" "+str(add_count-1)+":"+"Typos in register name.")                
+
+                        break
 
                 #for Type D instruction
                 if x[0]=="ld" or x[0]=="st":
@@ -142,27 +150,37 @@ else:
                     if(x[1] not in REG_ADD.keys()):
                         flag=1
                         if x[1]=="FLAGS":
-                                print("Illegal use of FLAGS register.")
+                                print("Line"+" "+str(add_count-1)+":"+"Illegal use of FLAGS register.")
                         else:        
-                            print("Typos in register name.")
-
+                            print("Line"+" "+str(add_count-1)+":"+"Typos in register name.")
+                        break
                     if(x[2] not in Var.keys()):
                         flag=1
-                        print("Use of undefined variables.")
-
-                #for Type F instruction
-                if x[0]=="hlt" and c!=len(assemblyfile):
-                    print("hlt must be at the end")
+                        print("Line"+" "+str(add_count-1)+":"+"Use of undefined variables.")
+                        break
                 
                 #for Type E instruction
                 if x[0]=="jmp" or x[0]=="jlt" or x[0]=="jgt" or x[0]=="je":
                     
                     if((x[1] +":") not in Label_add.keys()):
                         flag=1
-                        print("Use of undefined labels.")
+                        print("Line"+" "+str(add_count-1)+":"+"Use of undefined labels.")
+                        break
                 else:           
                     if (x[0] not in Instruction.keys()):
-                        print("Typos in instruction name.")
+                        print("Line"+" "+str(add_count-1)+":"+"Typos in instruction name.")
+                        break
+                
+                #for Type F instruction
+                if x[0]=="hlt" and c!=len(assemblyfile):
+                    print("Line"+" "+str(add_count-1)+":"+"hlt must be at the end")
+                    flag=1
+
+                    break
+
+                
+                add_count+=1
+                
 
     if  c==len(assemblyfile):
         if len(x)==1 and x[0]!="hlt":
@@ -245,13 +263,12 @@ else:
                         print(Instruction[x[0]][0]+"00000000000")
                 
 
-
-
     
 
     
 
      
 
-
     
+
+
